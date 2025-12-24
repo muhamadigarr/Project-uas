f788e25335ba9d76683e806a5101e00caac2d684
muhamadigarr <muhamadigaralfaruq@gmail.com>
2025-12-24 11:14:24 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i =1;
+    do {
+        cout<<"universitas pamulang" << i << endl;
+        i++;
+
+    } while (i <= 10);
+
+    cout << "muhamadigar"<<endl;
+    return 0;
+}
